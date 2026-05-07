7e219b29f9592e9cf7154506ebf2052e9c370df8
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-05-07 19:08:08 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,11 +17,20 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
+    assign uio_out[6:0] = 0;
+    assign uio_oe[0]  = 0;// input-start signal
+    assign uio_oe[7]  = 1;// output-done signal
+    
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, uio_in[7],uio_in[6],uio_in[5],uio_in[4],uio_in[3],uio_in[2],uio_in[1],1'b0};
+
+    sqrt8 core(
+        .clk(clk),
+        .reset(!rst_n),
+        .start(uio_in[0]),
+        .data_in(ui_in),   // Integer input
+        .data_out(uo_out),  // Q4.4 Fixed-point output
+        .done(uio_out[7])
+);
 
 endmodule
